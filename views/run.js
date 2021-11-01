9c217685406ac3968acc10158550122722d897ef
Christelle PREMEL-CABIC <premelch@ensieta.ecole>
2021-11-01 16:42:14 +0100

    add white_noise

--- views/run.js
+++ views/run.js
@@ -11,8 +11,8 @@ function getHtml(callback) {
 		'<body>' ;
 		
 html += '<div class="ui text container">'+
-    '<h3 class="first">Running 10 minuts</h3>'+
-    '<p>It\'s time to get out for a jog ! To keep the rythme, you can play the folliwing video. </p>'+
+    '<h3 class="first">Running 10 minuts (or more)</h3>'+
+    '<p>It\'s time to get out for a jog ! To keep the rythme, you can play the following video. </p>'+
 	'<p>I also recommand the Nike Run Club application. With the Nike Run Club app, you can monitor your run but you can also find guided runs <a href="https://nikerunning.app.link/iXkttjfFPkb">here</a></p>'+
 	'<iframe width="560" height="315" src="https://www.youtube.com/embed/QxbJSe6ueoY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'+	
 	'<div class="ui two column centered grid"><button type="button" class="ui large labeled icon button teal" onclick="location.href=\'/run\'" title="Next" ><i class="arrow circle right icon"></i>Next</button></div>' +
