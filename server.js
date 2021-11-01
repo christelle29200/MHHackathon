393acb4e8532d261e514f146e01d04b400803898
Christelle PREMEL-CABIC <premelch@ensieta.ecole>
2021-11-01 14:19:56 +0100

    add path packet

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
+var path = require('path')
+var firstVue = require('./views/firstPage.js');
+
 const app = express()
 const port = process.env.PORT || 443
 
 app.use('/assets', express.static(path.join(__dirname, 'semantic')))
-var firstVue = require('./views/firstPage.js');
+
 
 app.get('/', (req, res) => {
   firstVue.getHtml(function(page) {
