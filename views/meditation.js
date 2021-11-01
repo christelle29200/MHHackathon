8b5fcc0ed06ffd5bb54e3e781cae29b65e144c44
Christelle PREMEL-CABIC <premelch@ensieta.ecole>
2021-11-01 16:04:26 +0100

    add meta for mobile view

--- views/meditation.js
+++ views/meditation.js
@@ -3,6 +3,7 @@ function getHtml(callback) {
 		'<html lang="fr">' +
 		'<head>' +
 		'	<meta charset="UTF-8">' +
+		'<meta name="viewport" content="width=device-width, initial-scale=1" /> '+
 		'    <title>MH practice</title>' +
 		'        <link rel="stylesheet" type="text/css" href="/assets/dist/semantic.min.css" />' +
 		'	<script src="https://cdn.jsdelivr.net/npm/vue"></script>' +
